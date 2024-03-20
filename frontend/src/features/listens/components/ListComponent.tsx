72f92a9c395f98c83493a576a9b6624fe0ca633b
frenchiejnr <frenchiejnr@gmail.com>
2024-03-20 17:46:10 +0000

    feat: tracks are joined by name-album-artist

--- frontend/src/features/listens/components/ListComponent.tsx
+++ frontend/src/features/listens/components/ListComponent.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { SpotifyLink } from "./SpotifyLink";
 import { Link } from "react-router-dom";
+import { getItemUrl } from "../utils";
 
 export const ListComponent: React.FC<{
   item: any;
-}> = ({ item }) => (
-  <div key={item} className="flex w-2/5 justify-between mx-auto">
-    <div className="flex">
-      <SpotifyLink url={item.url} />
-      <Link to="" target="" className="hover:bg-violet-400 ml-1">
-        <p>{item.name}</p>
-      </Link>
+}> = ({ item }) => {
+  const url = getItemUrl(item);
+  return (
+    <div key={item} className="flex w-2/5 justify-between mx-auto">
+      <div className="flex">
+        <SpotifyLink url={item.url} />
+        <Link to={url?.[0]} target="" className="hover:bg-violet-400 ml-1">
+          <p>{item.name}</p>
+        </Link>
+      </div>
+      <p>{item.count}</p>
     </div>
-    <p>{item.count}</p>
-  </div>
-);
+  );
+};
